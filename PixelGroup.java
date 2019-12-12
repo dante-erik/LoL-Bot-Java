c6adb77d810d6b4bd1ee76c2de26a1363a704f97
Dohinkus <dohinkus@gmail.com>
2019-12-12 10:30:15 -0600

    Remove method duplicate and change method

--- PixelGroup.java
+++ PixelGroup.java
@@ -10,6 +10,7 @@ public class PixelGroup
 		visible = false;
 	}
 	
+	/*
 	//updates the boolean visible of the PixelGroup
 	//visible becomes true if ALL Pixels in the PixelGroup are visible on the screen
 	public void updateBoolean()
@@ -21,7 +22,28 @@ public class PixelGroup
 		for(int i = 0; i < pixels.size(); i++)
 		{
 			//if the pixel being scanned is not visible, visible is false
-			if(!equalToScreen(pixels.get(i)))
+			if(!pixels.get(i).equalToScreen())
+			{
+				visible = false;
+				//loop is ended so the remaining pixels are not scanned, efficient
+				i = pixels.size();
+			}
+		}
+	}
+	*/
+	
+	//updates the boolean visible of the PixelGroup
+	//visible becomes true if ALL Pixels in the PixelGroup are visible with a tolerance for N difference in RGB values
+	public void updateBoolean(int tolerance)
+	{
+		//true until proven false
+		visible = true;
+		
+		//check if each Pixel is visible on the screen
+		for(int i = 0; i < pixels.size(); i++)
+		{
+			//if the pixel being scanned is not visible, visible is false
+			if(!pixels.get(i).equalToScreen(tolerance))
 			{
 				visible = false;
 				//loop is ended so the remaining pixels are not scanned, efficient
@@ -30,10 +52,19 @@ public class PixelGroup
 		}
 	}
 	
-	//return boolean of current state
+	//return boolean isVisible after updating boolean with 0 tolerance
 	public boolean isVisible()
 	{
-		updateBoolean();
+		updateBoolean(0);
+		
+		return visible;
+	}
+	
+	
+	//return boolean isVisible after updating boolean with N tolerance
+	public boolean isVisible(int tolerance)
+	{
+		updateBoolean(tolerance);
 		
 		return visible;
 	}
