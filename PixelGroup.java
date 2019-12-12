1ad4f36bf5de55e15d25dd040ca10fb5f699ec8f
Dohinkus <dohinkus@gmail.com>
2019-12-12 09:01:25 -0600

    Change PixelGroup method functions and var names

--- PixelGroup.java
+++ PixelGroup.java
@@ -1,31 +1,40 @@
 public class PixelGroup
 {
 	private ArrayList<Pixel> pixels = new ArrayList<Pixel>();
-	private boolean state;
+	private boolean visible;
 	
 	//PixelGroups must be given an ArrayList of Pixel to be created
 	public PixelGroup(ArrayList<Pixel> p)
 	{
 		pixels.addAll(p);
-		state = false;
+		visible = false;
 	}
 	
-	//updates the state of the PixelGroup
-	//state becomes true if ALL Pixels in the PixelGroup are visible on the screen
-	public void update()
+	//updates the boolean visible of the PixelGroup
+	//visible becomes true if ALL Pixels in the PixelGroup are visible on the screen
+	public void updateBoolean()
 	{
 		//true until proven false
-		state = true;
+		visible = true;
 		
 		//check if each Pixel is visible on the screen
 		for(int i = 0; i < pixels.size(); i++)
+		{
+			//if the pixel being scanned is not visible, visible is false
 			if(!equalToScreen(pixels.get(i)))
-				state = false;
+			{
+				visible = false;
+				//loop is ended so the remaining pixels are not scanned, efficient
+				i = pixels.size();
+			}
+		}
 	}
 	
 	//return boolean of current state
-	public boolean getState()
+	public boolean isVisible()
 	{
-		return state;
+		updateBoolean();
+		
+		return visible;
 	}
 }
