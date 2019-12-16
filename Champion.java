5287479149522a5b53b00e8e5f684a57114bcb56
Dohinkus <dohinkus@gmail.com>
2019-12-16 09:21:51 -0600

    Add core

--- Champion.java
+++ Champion.java
@@ -1,2 +1,49 @@
 public class Champion
 {
+	public Champion(){}
+	
+	public void startQueue()
+	{
+		
+	}
+	
+	public void acceptMatch()
+	{
+		
+	}
+	
+	public void selectChampion()
+	{
+		
+	}
+	
+	public void callRole()
+	{
+		
+	}
+	
+	public void selectSummonerSpells()
+	{
+		
+	}
+	
+	public void honorTeammate()
+	{
+		
+	}
+	
+	public void dismissLevelUp()
+	{
+		
+	}
+	
+	public void dismissQuestComplete()
+	{
+		
+	}
+	
+	public void playAgain()
+	{
+		
+	}
+}
