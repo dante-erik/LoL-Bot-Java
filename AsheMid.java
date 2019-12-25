2f2e9c8609b79a5c774826abc539781fbb732c97
Dohinkus <dohinkus@gmail.com>
2019-12-25 11:44:54 -0600

    Add methods

---
+++ AsheMid.java
@@ -0,0 +1,44 @@
+import java.awt.AWTException;
+
+public class AsheMid extends Champion
+{
+	public AsheMid() throws AWTException
+	{
+	}
+	
+	public void pickChampion()
+	{
+		
+	}
+	
+	public void callRole()
+	{
+		
+	}
+	
+	public void selectSummonerSpells()
+	{
+		
+	}
+	
+	public void retreat()
+	{
+		rp.mouseMove(0,0);
+		rp.mouseClick(1);
+	}
+	
+	public void buyItems();
+	{
+		
+	}
+	
+	public void attack()
+	{
+		
+	}
+	
+	public void castSpells()
+	{
+		
+	}
+}
