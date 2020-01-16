517d68824bddd247fa8d88ccfe5cd7fb0a6d674c
Dohinkus <dohinkus@gmail.com>
2020-01-16 09:16:21 -0600

    Add locked x,y feature

--- src/DisplayPixel.java
+++ src/DisplayPixel.java
@@ -11,18 +11,39 @@ public class DisplayPixel
 		Color c = new Color(0,0,0);
 		Robot r = new Robot();
 		
+		boolean lockedOnPixel = false;
+		
 		Scanner kb = new Scanner(System.in);
+		
+		System.out.print("Lock onto specific pixel x,y? (y/N): ");
+		if(kb.next().toLowerCase().equals("y"))
+		{
+			lockedOnPixel = true;
+			System.out.println("Enter the x value of specific pixel: ");
+			x = kb.nextInt();
+			System.out.println("Enter the y value of specific pixel: ");
+			y = kb.nextInt();
+		}
+		
 		System.out.print("Enter the delay in ms between prints (int): ");
 		int milliseconds = Math.abs(kb.nextInt());
 		
-		while(true)
-		{
-			p = MouseInfo.getPointerInfo().getLocation();
-			x = (int)Math.round(p.getX());
-			y = (int)Math.round(p.getY());
-			c = r.getPixelColor(x, y);
-			System.out.println(x+", "+y+", "+c.getRed()+", "+c.getGreen()+", "+c.getBlue());
-			r.delay(milliseconds);
-		}
+		if(lockedOnPixel)
+			while(true)
+			{
+				c = r.getPixelColor(x, y);
+				System.out.println(x+", "+y+", "+c.getRed()+", "+c.getGreen()+", "+c.getBlue());
+				r.delay(milliseconds);
+			}
+		else
+			while(true)
+			{
+				p = MouseInfo.getPointerInfo().getLocation();
+				x = (int)p.getX();
+				y = (int)p.getY();
+				c = r.getPixelColor(x, y);
+				System.out.println(x+", "+y+", "+c.getRed()+", "+c.getGreen()+", "+c.getBlue());
+				r.delay(milliseconds);
+			}
 	}
 }
