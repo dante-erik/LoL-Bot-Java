b935859b661e73f1124e019abe53eea43ab38de8
Dohinkus <dohinkus@gmail.com>
2020-01-16 09:50:14 -0600

    Change champion select methods slowed down for consistency in selecting correct champion, calling role, and locking in

--- src/Champion.java
+++ src/Champion.java
@@ -9,31 +9,44 @@ public class Champion
 	
 	public void selectChampion(String championName)
 	{
-		rp.setAutoDelay(75);
+		//accuracy over speed
+		rp.setAutoDelay(125);
 		//text box for champ search
 		rp.mouseMove(1087, 264);
+		rp.mouseClick(1);
 		rp.mouseClick(1);
 		rp.keyClick(championName);
 		//icon for champ searched
 		rp.mouseMove(702, 330);
 		rp.mouseClick(1);
+		rp.mouseClick(1);
+		//no delay for other methods
+		rp.setAutoDelay(0);
 	}
 	
 	public void callRole(String role)
 	{
+		//accuracy over speed
+		rp.setAutoDelay(75);
 		//text bot for msging other players
 		rp.mouseMove(387, 842);
 		rp.mouseClick(1);
 		rp.keyClick(role);
 		rp.keyPress(KeyEvent.VK_ENTER);
 		rp.keyRelease(KeyEvent.VK_ENTER);
+		//no delay for other methods
+		rp.setAutoDelay(0);
 	}
 	
 	public void lockIn()
 	{
+		//accuracy over speed
+		rp.setAutoDelay(75);
 		//lock in button
 		rp.mouseMove(963, 766);
 		rp.mouseClick(1);
+		//no delay for other methods
+		rp.setAutoDelay(0);
 	}
 	
 	public void lockCamera()
