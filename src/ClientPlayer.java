7f6970548f275005423e7d3cf6200e862e9c8dd6
Dohinkus <dohinkus@gmail.com>
2020-01-22 09:06:25 -0600

    Add new method and more comments

--- src/ClientPlayer.java
+++ src/ClientPlayer.java
@@ -8,49 +8,60 @@ public class ClientPlayer
 	
 	public void startQueue()
 	{
+		//start queue button
 		rp.mouseMove(858, 846);
 		rp.mouseClick(1);
 	}
 	
 	public void acceptMatch()
 	{
+		//accept button
 		rp.mouseMove(957, 717);
 		rp.mouseClick(1);
 	}
 	
 	public void honorTeammate()
 	{
+		//teammate portrait
 		rp.mouseMove(1138, 521);
-		rp.mouseClick(1);
-		rp.mouseClick(1);
-		rp.mouseClick(1);
-		rp.mouseClick(1);
 		rp.mouseClick(1);
 	}
 	
 	public void dismissLevelUp()
 	{
+		//ok button
 		rp.mouseMove(958, 839);
 		rp.mouseClick(1);
 	}
 	
 	public void dismissMissions()
 	{
+		//ok button
 		rp.mouseMove(961, 838);
 		rp.mouseClick(1);
 	}
 	
 	public void playAgain()
 	{
+		//play again button
 		rp.mouseMove(855, 848);
 		rp.mouseClick(1);
 	}
 	
 	public void acceptDailyReward()
 	{
+		//champion portrait's select button
 		rp.mouseMove(955, 548);
 		rp.mouseClick(1);
+		//ok button
 		rp.mouseMove(960, 837);
+		rp.mouseClick(1);
+	}
+	
+	public void declineRunesSaveRequest()
+	{
+		//No button
+		rp.mouseMove(994, 551, 33, 37, 42);
 		rp.mouseClick(1);
 	}
 }
